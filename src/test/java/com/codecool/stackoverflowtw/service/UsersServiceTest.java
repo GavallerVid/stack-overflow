fea29fe8365654856e322a2bb9abd47213193557
gavaller-vid <gavalleratilla@gmail.com>
2023-09-05 13:30:42 +0200

    test: addNewUser from UserService

---
+++ src/test/java/com/codecool/stackoverflowtw/service/UsersServiceTest.java
@@ -0,0 +1,19 @@
+package com.codecool.stackoverflowtw.service;
+
+import com.codecool.stackoverflowtw.dao.UsersDaoJdbc;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersServiceTest {
+    @Test
+    void addNewUser() {
+        PSQLConnect connect = new PSQLConnect();
+        UsersDaoJdbc usersDaoJdbc = new UsersDaoJdbc(connect);
+        UsersService usersService = new UsersService(usersDaoJdbc);
+        assertEquals(2, usersService.addNewUser(2,"Neo", "fsdf"));
+    }
+
+}
