e5b3a8b65f0598e4c3a546df0a1f46f4a273d5b3
breezh <benii1030@gmail.com>
2023-09-05 10:54:57 +0200

    create psql connection

---
+++ src/main/java/com/codecool/stackoverflowtw/service/PSQLConnect.java
@@ -0,0 +1,23 @@
+package com.codecool.stackoverflowtw.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PSQLConnect {
+    private final String url = "jdbc:postgresql://localhost:5432/stackoverflow";
+    private final String user = "postgres";
+    private final String password = "<add your password>";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+}
