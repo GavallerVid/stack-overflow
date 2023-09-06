8f6cfe4bfae3b1068f66fe62b8b59d713c2c6fe6
pannaincze <pannaincze@gmail.com>
2023-09-06 14:38:10 +0200

    fix variable and parameter issues in AnswerDaoJdbc

--- src/main/java/com/codecool/stackoverflowtw/dao/AnswerDao.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/AnswerDao.java
@@ -1,15 +1,13 @@
 package com.codecool.stackoverflowtw.dao;
 
 import com.codecool.stackoverflowtw.controller.dto.AnswerDTO;
-import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
 import com.codecool.stackoverflowtw.dao.model.Answer;
-import com.codecool.stackoverflowtw.dao.model.Question;
 
 import java.util.List;
 
 public interface AnswerDao {
 
-    List<Answer> getAll(int questionId);
+    List<Answer> getAllAnswerByQuestionId(int questionId);
     Answer getById(int id);
     int deleteById(int id);
     int add(AnswerDTO answer);
