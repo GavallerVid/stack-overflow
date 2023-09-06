16feab5b54239c19c4b104fe6739b1f1e3765f50
breezh <benii1030@gmail.com>
2023-09-06 14:15:44 +0200

    finish implementing addQuestion

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,7 +1,9 @@
-
-
-INSERT INTO questions
-VALUES (1, 'random question1', 1);
+INSERT INTO users
+VALUES (1, 'user1', 'puser1');
 
 INSERT INTO questions
-VALUES (2, 'random question2', 1)
+VALUES (1, 'random title1', 'random question1', 1);
+
+INSERT INTO questions
+VALUES (2, 'random title2', 'random question2', 1);
+
