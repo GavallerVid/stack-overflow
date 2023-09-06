9a25839d5ddad4c3ddf7a741b5f1df95cc2495e4
gavaller-vid <gavalleratilla@gmail.com>
2023-09-06 15:02:21 +0200

    refactor NewQuestionForm.jsx

--- src/main/ui/src/Components/NewQuestionForm.jsx
+++ src/main/ui/src/Components/NewQuestionForm.jsx
@@ -1,11 +1,27 @@
+import {useState} from "react";
+
 export default function NewQuestionForm() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const questionTitle = event.target.questionTitle;
+        const questionDescription = event.target.questionDescription;
+        setInputs(values => ({...values, [questionTitle]: questionDescription}))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs.questionTitle)
+    }
+
     return (
         <div id={"formContainer"}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="question-title">Question Title:</label>
-                <input type="text" id="question-title" name="question-title"/>
+                <input type="text" id="questionTitle" name="questionTitle"
+                value={inputs.questionTitle} onChange={handleChange}/>
                 <label htmlFor="question-description">Question:</label>
-                <input type="text" id="question-description" name="question-description"/>
+                <input type="text" id="questionDescription" name="questionDescription"
+                       value={inputs.questionDescription} onChange={handleChange}/>
                 <input type={"submit"}/>
             </form>
         </div>
