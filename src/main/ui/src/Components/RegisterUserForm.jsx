9c0b479a3bdec66c953bb7eb14c30bed8cf3aad8
breezh <benii1030@gmail.com>
2023-09-07 11:29:15 +0200

    finish implementing register user

--- src/main/ui/src/Components/RegisterUserForm.jsx
+++ src/main/ui/src/Components/RegisterUserForm.jsx
@@ -1,19 +1,53 @@
 import {Form} from "react-bootstrap";
+import {useState} from "react";
 export default function RegisterUserForm() {
+
+    const [userName, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameInput (event) {
+        setUsername(event.target.value)
+    }
+    function handlePasswordInput (event) {
+        setPassword(event.target.value)
+    }
+
+
+    async function registerUser (enteredUsername, enteredPassword) {
+        const body = {
+            username : enteredUsername,
+            password : enteredPassword
+        }
+        const response = await fetch('/users/', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const result = await response.json();
+        console.log(result);
+        console.log(response.status)
+    }
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        registerUser(userName, password);
+    }
 
     return (
         <Form>
             <div className="form-group">
                 <label htmlFor="username">Username</label>
-                <input type="username" className="form-control" id="username" aria-describedby="emailHelp"
+                <input onChange={handleUsernameInput} type="username" className="form-control" id="username" aria-describedby="emailHelp"
                        placeholder="Enter username"/>
                     <small id="usernameHelp" className="form-text text-muted">Think about a unique username</small>
             </div>
             <div className="form-group">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                <input onChange={handlePasswordInput} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
-            <button onClick={} type="submit" className="btn btn-primary">Register</button>
+            <button onClick={handleSubmit} type="submit" className="btn btn-primary">Register</button>
         </Form>
     )
 }
