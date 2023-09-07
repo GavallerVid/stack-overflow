07d2b84a5da6054baa573b60d757c16a6ca73794
breezh <benii1030@gmail.com>
2023-09-07 15:26:16 +0200

    fixing login error

--- src/main/ui/src/Pages/LoginPage.jsx
+++ src/main/ui/src/Pages/LoginPage.jsx
@@ -6,8 +6,8 @@ import Layout from "../Layout";
 import {useUser} from "../Components/UserContext";
 
 //url should not contain the password, but it's okay for now
-function fetchUser(username,password){
-    return fetch("/users/"+username+"+"+password).then((res) => res.json());
+function fetchUser(username, password) {
+    return fetch("/users/" + username + "+" + password).then((res) => res.json());
 }
 export default function LoginPage() {
     const {handleLogin} = useUser();
@@ -17,18 +17,22 @@ export default function LoginPage() {
     const [password, setPassword] = useState('');
 
     if (loading) {
-        return <Loading />
+        return <Loading/>
     }
 
-    function handleLoginCLick(){
+    function handleLoginCLick() {
         setLoading(true)
-        fetchUser(username,password).then((user) => {
+        fetchUser(username, password).then((user) => {
             setLoading(false);
-            handleLogin(user)
-            navigate('/')
+            if (user.id > 0) {
+                handleLogin(user)
+                navigate('/')
+            } else {
+                navigate('/users/login')
+            }
         });
         if (loading) {
-            return <Loading />
+            return <Loading/>
         }
 
     }
@@ -44,16 +48,22 @@ export default function LoginPage() {
     return (
         <Form>
             <div className="position-absolute top-50 start-50 translate-middle">
-            <div className="mb-3">
-                <label htmlFor="username">Username</label>
-                <input onChange={(event) => handleUsernameChange(event)} type="username" className="form-control" id="username" aria-describedby="emailHelp"
-                       placeholder="Enter username"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input onChange={(event) => handlePasswordChange(event)} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-            </div>
-                <button onClick={event => {event.preventDefault(); handleLoginCLick()}} type="submit" className="btn btn-secondary btn-lg">Login</button>
+                <div className="mb-3">
+                    <label htmlFor="username">Username</label>
+                    <input onChange={(event) => handleUsernameChange(event)} type="username" className="form-control"
+                           id="username" aria-describedby="emailHelp"
+                           placeholder="Enter username"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input onChange={(event) => handlePasswordChange(event)} type="password" className="form-control"
+                           id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <button onClick={event => {
+                    event.preventDefault();
+                    handleLoginCLick()
+                }} type="submit" className="btn btn-secondary btn-lg">Login
+                </button>
             </div>
         </Form>
     )
