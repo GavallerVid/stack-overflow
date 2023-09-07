e4c026f73c1ddc3566df8da46178267745159d60
gavaller-vid <gavalleratilla@gmail.com>
2023-09-07 10:22:26 +0200

    refactor NewQuestionForm.jsx to handle title and description changes separately.

--- src/main/ui/src/Components/NewQuestionForm.jsx
+++ src/main/ui/src/Components/NewQuestionForm.jsx
@@ -1,27 +1,30 @@
 import {useState} from "react";
 
 export default function NewQuestionForm() {
-    const [inputs, setInputs] = useState({});
 
-    const handleChange = (event) => {
-        const questionTitle = event.target.questionTitle;
-        const questionDescription = event.target.questionDescription;
-        setInputs(values => ({...values, [questionTitle]: questionDescription}))
+    const [questionTitle, setQuestionTitle] = useState("");
+    const [questionDescription, setQuestionDescription] = useState("");
+    const handleQuestionTitleChange = (event) => {
+        setQuestionTitle(event.target.value)
+    }
+    const handleQuestionDescriptionChange = (event) => {
+        setQuestionDescription(event.target.value)
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(inputs.questionTitle)
+        console.log(questionTitle + " " +questionDescription)
     }
 
     return (
         <div id={"formContainer"}>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="question-title">Question Title:</label>
-                <input type="text" id="questionTitle" name="questionTitle"
-                value={inputs.questionTitle} onChange={handleChange}/>
+                <input type="text" id={"questionTitle"} name={"questionTitle"}
+                       defaultValue={questionTitle} onChange={handleQuestionTitleChange}/>
                 <label htmlFor="question-description">Question:</label>
-                <input type="text" id="questionDescription" name="questionDescription"
-                       value={inputs.questionDescription} onChange={handleChange}/>
+                <input type="text" id={"questionDescription"} name={"questionDescription"}
+                       defaultValue={questionDescription}
+                       onChange={handleQuestionDescriptionChange}/>
                 <input type={"submit"}/>
             </form>
         </div>
