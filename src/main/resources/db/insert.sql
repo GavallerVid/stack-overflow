5f903f386bec41c80fae8063691c73af4534995a
pannaincze <pannaincze@gmail.com>
2023-09-07 14:28:57 +0200

    update insert.sql

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -3,10 +3,10 @@ VALUES (1,'name','pass');
 
 
 INSERT INTO questions
-VALUES (1,'title1','random question1', 1);
+VALUES (1,'title1','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lorem elit, blandit non ipsum ultrices, bibendum dignissim diam. Fusce dapibus ante consequat ultricies elementum. Donec volutpat sem tellus, quis malesuada dui porttitor nec. Aenean vitae ipsum vel turpis ullamcorper ultricies. Sed sed tempor neque, ut condimentum neque. Praesent quis erat id dui sagittis dictum. Morbi tristique leo ex, ac venenatis felis cursus in. Sed lobortis tempus bibendum. Etiam tristique auctor varius. Curabitur condimentum consectetur dapibus. Vestibulum vehicula, lorem vel congue malesuada, purus dui laoreet nulla, et ultricies purus nisi sed est. Nulla vel porta tellus, ac consectetur magna. Integer ut risus et lectus sollicitudin mollis sed sed urna.', 1);
 
 INSERT INTO questions
-VALUES (2,'title2', 'random question2', 1);
+VALUES (2,'title2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lorem elit, blandit non ipsum ultrices, bibendum dignissim diam. Fusce dapibus ante consequat ultricies elementum. Donec volutpat sem tellus, quis malesuada dui porttitor nec. Aenean vitae ipsum vel turpis ullamcorper ultricies. Sed sed tempor neque, ut condimentum neque. Praesent quis erat id dui sagittis dictum. Morbi tristique leo ex, ac venenatis felis cursus in. Sed lobortis tempus bibendum. Etiam tristique auctor varius. Curabitur condimentum consectetur dapibus. Vestibulum vehicula, lorem vel congue malesuada, purus dui laoreet nulla, et ultricies purus nisi sed est. Nulla vel porta tellus, ac consectetur magna. Integer ut risus et lectus sollicitudin mollis sed sed urna.', 1);
 
 INSERT INTO answers
 VALUES (1, 'random', 1, 1);
